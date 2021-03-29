b51d502b25bca631cae02fea60efd7f2eac6835c
Ashwina7999 <59051731+Ashwina7999@users.noreply.github.com>
2021-03-29 20:01:20 +0530

    Add files via upload

---
+++ building_automation1/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:building_automation/floor_page.dart';
+import 'package:building_automation/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:building_automation/block_page.dart';
+import 'package:building_automation/classroom.dart';
+import 'package:building_automation/fan_light.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        // '/': (context) => Login(),
+        '/': (context) => Login(),
+        '/floor': (context) => FloorPage(),
+        '/class': (context) => ClassRoom(),
+        '/status': (context) => FanLight(),
+      },
+    );
+  }
+}
